7033c0e7dc834466575a9b7cc8187ff162cdd213
hommym <herbertharthur80@gmail.com>
2024-12-19 00:09:31 +0000

    Updated Error Handler

--- src/common/middlewares/errorHandler.ts
+++ src/common/middlewares/errorHandler.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { MulterError } from "multer";
 
 export class AppError extends Error {
   message: string;
@@ -16,6 +17,8 @@ export const errorHandler = async (err: Error, req: Request, res: Response, next
     res.status(err.statusCode).json({ error: err.message });
   } else if (err instanceof SyntaxError) {
     res.status(400).json({ error: err.message });
+  } else if (err instanceof MulterError) {
+    res.status(400).json({ error: `${err.message} for uploaded file` });
   } else {
     console.log(err);
     res.status(500).json({ error: "Server Error" });
