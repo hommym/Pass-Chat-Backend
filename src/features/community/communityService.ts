6abe05ec8844537b04d3bc039955c0fbfbdedb1e
hommym <herbertharthur80@gmail.com>
2025-01-07 20:12:14 +0000

    Community Routes(Update): 1. Modified the endpoints for creating groups and channels to a create or update endpoint

--- src/features/community/communityService.ts
+++ src/features/community/communityService.ts
@@ -1,29 +1,33 @@
-import { Community } from "@prisma/client";
+import { ChatRoom, Community } from "@prisma/client";
 import { CreateCommunityDto } from "./dto/createCommunityDto";
 import { database } from "../../common/constants/objects";
 import { AppError } from "../../common/middlewares/errorHandler";
 
 export class CommunityService {
-  async checkCommunity(type: "channel" | "group", name: string) {
-    return await database.community.findUnique({ where: { type_name: { name, type } } });
+  async checkCommunity(type: "channel" | "group", name: string, ownerId: number) {
+    return await database.community.findUnique({ where: { type_name: { name, type }, ownerId } });
   }
 
   async createCommunity(type: "channel" | "group", communityDto: CreateCommunityDto, ownerId: number) {
     const { name, description, visibility, profile } = communityDto;
     let permissions: any;
-    if (await this.checkCommunity(type, name)) throw new AppError(`A ${type} with this name already exist`, 409);
-    const chatRoom = await database.chatRoom.create({ data: { type, name } });
-
-    if (type === "group") {
-      permissions = {
-        messaging: "all",
-        mediaSharing: "all",
-        communitySharing: "all",
-        polls: "all",
-        pinning: "admins",
-      };
+    let chatRoom: ChatRoom;
+    if (!(await this.checkCommunity(type, name, ownerId))) {
+      chatRoom = await database.chatRoom.create({ data: { type, name } });
+      if (type === "group") {
+        permissions = {
+          messaging: "all",
+          mediaSharing: "all",
+          communitySharing: "all",
+          polls: "all",
+          pinning: "admins",
+        };
+      }
     }
-
-    return await database.community.create({ data: { name, type, description, visibility, permissions, profile, roomId: chatRoom.id, ownerId } });
+    return await database.community.upsert({
+      where: { type_name: { name, type }, ownerId },
+      create: { name, type, description, visibility, permissions, profile, roomId: chatRoom!?chatRoom.id:0, ownerId },
+      update: { name, type, description, visibility, profile },
+    });
   }
 }
