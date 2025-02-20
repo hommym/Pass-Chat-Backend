9e0d858d605b8ba62032ed321d52af7674855ee1
hommym <herbertharthur80@gmail.com>
2025-02-20 12:27:29 +0000

    Code Refactor: 1.Added a new library uuid 2.Modified the values for content property in message seeder

--- src/common/database/seeders/messageSeeder.ts
+++ src/common/database/seeders/messageSeeder.ts
@@ -1,6 +1,6 @@
 import { MessageType } from "@prisma/client";
-import { database } from "../../constants/objects";
-
+import { database, randomData } from "../../constants/objects";
+import { v4 } from "uuid";
 export const MessageSeeder = async () => {
   const chatRooms = await database.chatRoom.findMany({ where: { user1Id: { not: null }, user2Id: { not: null } }, select: { id: true, user1Id: true, user2Id: true } });
 
@@ -11,14 +11,14 @@ export const MessageSeeder = async () => {
 
   const messages = [];
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < chatRooms.length; i++) {
     const chatRoom = chatRooms[Math.floor(Math.random() * chatRooms.length)];
     const senderId = Math.random() < 0.5 ? chatRoom.user1Id : chatRoom.user2Id;
     const recipientId = senderId === chatRoom.user1Id ? chatRoom.user2Id : chatRoom.user1Id;
 
     messages.push({
       type: "text" as MessageType,
-      content: { text: `Sample message ${i + 1}` },
+      content: { text: JSON.stringify({ content: randomData.num(0, 1) === 1 ? "Hello From this side" : "Hi , nice to meet you", content_id: v4() }) },
       senderId: senderId!,
       recipientId,
       roomId: chatRoom.id,
