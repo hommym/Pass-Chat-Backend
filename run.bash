addd6c637de81893b4a10b585278b6decbe0472a
hommym <herbertharthur80@gmail.com>
2025-09-23 23:40:07 +0000

    feat: add run script and update start command
    This commit introduces a `run.bash` script to automate the
    setup and execution of the application. The script checks for
    the presence of `node_modules` and `.env` files, installs
    dependencies if needed, and then builds and starts the server.
    
    The `start` script in `package.json` has also been updated to
    remove the `tsc` command, as the `run.bash` script now handles
    the build process.

---
+++ run.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+isPackagesInstalled=0
+isEnvPresent=0
+
+
+for item in $(ls -a)
+do
+    if [ $item == "node_modules" ]; then
+        isPackagesInstalled=1
+
+    elif [ $item == ".env" ]; then 
+        isEnvPresent=1;
+
+    fi
+done 
+
+# checking if dependencies have been installed
+if [ $isPackagesInstalled == 0 ]; then
+    npm install
+fi
+
+
+#check if env avialable before continuing
+if [ $isEnvPresent == 0 ]; then 
+    echo "No .env file present"
+
+else 
+    #building the project
+    tsc
+    #start http server
+    npm start
+fi
+
+
+
+
+
