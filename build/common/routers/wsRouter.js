82a589bd355111f87d8bd758bcad3a70d2389981
hommym <herbertharthur80@gmail.com>
2025-10-05 18:42:40 +0000

    refactor: add system flag to wsRouter for cross-server msgs

--- build/common/routers/wsRouter.js
+++ build/common/routers/wsRouter.js
@@ -4,9 +4,15 @@ exports.wsRouter = void 0;
 const chatHandler_1 = require("../../features/chat/ws/chatHandler");
 const chatNotificationHandler_1 = require("../../features/notification/ws/chatNotificationHandler");
 const authHandler_1 = require("../../features/auth/ws/authHandler");
-const wsRouter = (mainPath) => {
-    (0, chatHandler_1.chatRouterDef)(mainPath);
-    (0, chatNotificationHandler_1.notificationRouterDef)(mainPath);
-    (0, authHandler_1.authRouterDef)(mainPath);
+const crossMsgRouterHandler_1 = require("../../sys_features/cross_msg/ws/crossMsgRouterHandler");
+const wsRouter = (mainPath, isSystem = false) => {
+    if (isSystem) {
+        (0, crossMsgRouterHandler_1.crossServerMsgDef)(mainPath);
+    }
+    else {
+        (0, chatHandler_1.chatRouterDef)(mainPath);
+        (0, chatNotificationHandler_1.notificationRouterDef)(mainPath);
+        (0, authHandler_1.authRouterDef)(mainPath);
+    }
 };
 exports.wsRouter = wsRouter;
--- src/common/routers/wsRouter.ts
+++ src/common/routers/wsRouter.ts
@@ -1,9 +1,15 @@
 import { chatRouterDef } from "../../features/chat/ws/chatHandler";
 import { notificationRouterDef } from "../../features/notification/ws/chatNotificationHandler";
 import { authRouterDef } from "../../features/auth/ws/authHandler";
+import { crossServerMsgDef } from "../../sys_features/cross_msg/ws/crossMsgRouterHandler";
 
-export const wsRouter = (mainPath: string) => {
-  chatRouterDef(mainPath);
-  notificationRouterDef(mainPath);
-  authRouterDef(mainPath);
+export const wsRouter = (mainPath: string,isSystem=false) => {
+  if(isSystem){
+    crossServerMsgDef(mainPath)
+  }else{
+    chatRouterDef(mainPath);
+    notificationRouterDef(mainPath);
+    authRouterDef(mainPath);
+  }
+  
 };
